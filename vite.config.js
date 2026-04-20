bf705182498cccd2233f7189dc7973a4851b69b2
authcar@gmail.com <youremail@example.com>
2026-04-20 14:14:52 +0700

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        page2: resolve(__dirname, "index2.html"),
+        page3: resolve(__dirname, "index3.html"),
+        page4: resolve(__dirname, "index4.html"),
+      },
+    },
+  },
 });
